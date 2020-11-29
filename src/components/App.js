07b95e6e5b6ea9769df4c0900d7038c445452bf8
Ryan Gonzales <helloryantg@gmail.com>
2020-11-28 18:59:31 -0800

    add partyCount variable

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,24 @@
 import './App.css'
+import { useState, useEffect } from 'react'
 
 function App() {
+  const [partyCount, setPartyCount] = useState(2)
+
+  const handleChangePartyCount = (partyCount) => {
+    setPartyCount(partyCount)
+  }
+
   return (
     <div className="App">
       <div className="header">
         <label>How many people are in your party?</label>
-        <input type="number" min="2" max="20" />
+        <input
+          type="number"
+          min="2"
+          max="20"
+          value={partyCount}
+          onChange={({ target }) => handleChangePartyCount(target.value)}
+        />
       </div>
     </div>
   );
