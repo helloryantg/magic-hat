215ef9a6763f66aaf8fc8ab8a562c32d0b6fee19
helloryantg <helloryantg@gmail.com>
2020-11-30 18:18:53 -0800

    add handle click functionality for person count

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,17 @@
 import "./App.css"
 import { useState, useEffect } from "react"
 
-const partyCountChoices = [2, 3, 4, 5, 6, 7, 8, 9, 10]
+const partyCountChoices = [2, 3, 4, 5, 6, 7, 8, 9]
 
 function App() {
   const [partyCount, setPartyCount] = useState(2)
+  const [isAddingCustomCount, setAddingCustomCount] = useState(false)
+  
+  const handleChoiceClick = ({ target }) => {
+    setPartyCount(target.textContent)
+  }
+
+  console.log(partyCount)
 
   return (
     <div className="App">
@@ -12,8 +19,9 @@ function App() {
         <label>How many people are in your party?</label>
         <div className="choices">
           {partyCountChoices.map((count) => {
-            return <span className="choice">{count}</span>
+            return <span className="choice" onClick={handleChoiceClick}>{count}</span>
           })}
+          <span className="choice">10+</span>
         </div>
       </div>
     </div>
